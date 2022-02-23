dcb3251f03a139669023bfcb564164a33d8ce6bb
Claudio <claudiodicaprio89@gmail.com>
2022-02-23 14:05:54 +0100

    first commit

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+      <h2>I'm the header</h2>
+     <a href="/admin/home"><button >Reserved Area</button></a>
+  </header>
+</template>
+
+<script>
+export default {
+    name:"Header"
+}
+</script>
+
+<style>
+
+</style>
